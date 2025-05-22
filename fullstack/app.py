544a7d8e7ac39fbe19c9d34783ebd0ac2e16d9b5
omrivilan <136691490+omrivilan@users.noreply.github.com>
2025-05-22 08:15:00 +0000

    moveee the buttonnnn

--- fullstack/app.py
+++ fullstack/app.py
@@ -182,8 +182,8 @@ def display_login():
 
     # Sign up section
     # Center "Don't have an account?" and Sign Up button together
-    col1, col2, col3 = st.columns([1, 2, 1])
-    with col2:
+    col1, col2, col3, col4, col5, col6, col7 = st.columns([1,1,1, 2, 1,1,1])
+    with col5:
         st.markdown('<div class="signup-label">Don\'t have an account?</div>', unsafe_allow_html=True)
         st.markdown('<div class="center-signup">', unsafe_allow_html=True)
         if st.button("Sign Up", key="signup_trigger"):
