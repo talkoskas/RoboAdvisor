90ca4c0b1c2166c794ab30794e6885e500879e13
omrivilan <136691490+omrivilan@users.noreply.github.com>
2025-06-04 15:32:04 +0300

    Update app.py

--- fullstack/app.py
+++ fullstack/app.py
@@ -17,7 +17,6 @@ PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir
 if PROJECT_ROOT not in sys.path:
     sys.path.insert(0, PROJECT_ROOT)
 
-# — now the import will work —
 from MainStreamlit_forecast import AppManager
 
 
